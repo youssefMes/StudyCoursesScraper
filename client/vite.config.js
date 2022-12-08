e0bec52e731d92567463d30a8e7a76483d941a9e
youssefMes <youssef-mestiri@outlook.fr>
2022-12-08 23:07:30 +0100

    dockerize client

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,9 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [react(), svgr()]
 })
