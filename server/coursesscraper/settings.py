e2e491090894ed16badbbd021c9483a69380cb48
youssefMes <youssef-mestiri@outlook.fr>
2022-12-29 14:08:32 +0100

    cache clear command , add scrapy app as fjango app

--- server/coursesscraper/settings.py
+++ server/coursesscraper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'scraper',
     'scraperApp',
     'corsheaders',
     'rest_framework',
