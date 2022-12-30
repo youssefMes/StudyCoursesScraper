34a42666a132fb14a6be57c3a317904bd4b235e8
youssefMes <youssef-mestiri@outlook.fr>
2022-12-30 21:02:50 +0100

    change localhost to service name

--- server/scraperApp/tests.py
+++ server/scraperApp/tests.py
@@ -21,33 +21,33 @@ class CourseTestCase(TestCase):
 
     def test_course_results(self):
         """courses page loads properly"""
-        response = self.client.get('http://localhost:8000/api/courses/')
+        response = self.client.get('http://server:8000/api/courses/')
         self.assertEqual(response.status_code, 200)
 
     def test_course_results_search(self):
         """search fonctionalities"""
-        response = self.client.get('http://localhost:8000/api/courses/', {'search': 'Management'})
+        response = self.client.get('http://server:8000/api/courses/', {'search': 'Management'})
         for result in response.data['results']:
             self.assertIn('Management', result['name'])
     
     def test_course_results_filter(self):
         """fitler fonctionalities"""
-        response = self.client.get('http://localhost:8000/api/courses/', {'city': 'CITY_1'})
+        response = self.client.get('http://server:8000/api/courses/', {'city': 'CITY_1'})
         self.assertEqual(len(response.data['results']), 1)
         for result in response.data['results']:
             self.assertIn('CITY_1', result['information']['city'])    
 
-        response = self.client.get('http://localhost:8000/api/courses/', {'city': 'CITY_1', 'portal': 'Test Portal 1'})
+        response = self.client.get('http://server:8000/api/courses/', {'city': 'CITY_1', 'portal': 'Test Portal 1'})
         self.assertEqual(len(response.data['results']), 1)
         self.assertIn('CITY_1', response.data['results'][0]['information']['city'])
         self.assertEqual('Test Portal 1', response.data['results'][0]['portal']['name'])
 
-        response = self.client.get('http://localhost:8000/api/courses/', {'city': 'CITY_1', 'portal': 'Test Portal 2'})
+        response = self.client.get('http://server:8000/api/courses/', {'city': 'CITY_1', 'portal': 'Test Portal 2'})
         self.assertEqual(len(response.data['results']), 0)
         
     def test_course_pagination(self):
         """ Pagination """
-        response = self.client.get('http://localhost:8000/api/courses/', {'limit': 2})
+        response = self.client.get('http://server:8000/api/courses/', {'limit': 2})
         self.assertIn('page=2', response.data['next'])
         self.assertEqual(3, response.data['count'])
 
@@ -66,24 +66,24 @@ class BookmarkTestCase(TestCase):
         cls.user.save()
         Bookmark.objects.create(user = cls.user, course= cls.course_1)
         Bookmark.objects.create(user = cls.user, course= cls.course_2)
-        resp = cls.api_client.post('http://localhost:8000/api/jwt/create/', {'email': cls.user.email, 'password': '12345678'}, format='json')
+        resp = cls.api_client.post('http://server:8000/api/jwt/create/', {'email': cls.user.email, 'password': '12345678'}, format='json')
         cls.token = resp.data['access']
                 
     def test_bookmark_creation(self):
         """Create bookmark"""
-        response = self.api_client.post('http://localhost:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_3.id}, format='json')
+        response = self.api_client.post('http://server:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_3.id}, format='json')
         self.assertEqual(response.status_code, 401)
         self.api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
-        response = self.api_client.post('http://localhost:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_1.id}, format='json')
+        response = self.api_client.post('http://server:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_1.id}, format='json')
         self.assertEqual(response.status_code, 400)
-        response = self.api_client.post('http://localhost:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_3.id}, format='json')
+        response = self.api_client.post('http://server:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_3.id}, format='json')
         self.assertEqual(response.status_code, 201)
 
 
     def test_bookmarks_listing(self):
         """retrieve user bookmarks"""
         self.api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
-        response = self.api_client.get('http://localhost:8000/api/bookmarks/', {'page': 2, 'limit': 1}, format='json')
+        response = self.api_client.get('http://server:8000/api/bookmarks/', {'page': 2, 'limit': 1}, format='json')
         self.assertEqual(response.status_code, 200)        
         self.assertEqual(2, response.data['count'])
         self.assertIn('page=1', response.data['previous'])
@@ -115,28 +115,28 @@ class UserTestCase(TestCase):
         user = User.objects.create_user(**cls.current_user)
         user.is_active = True
         user.save()
-        response = cls.api_client.post('http://localhost:8000/api/jwt/create/', cls.current_user , format='json')
+        response = cls.api_client.post('http://server:8000/api/jwt/create/', cls.current_user , format='json')
         cls.token = response.data['access']
                 
     def test_user_registration(self):
         """user register"""
-        response = self.api_client.post('http://localhost:8000/api/users/', self.user_register, format='json')
+        response = self.api_client.post('http://server:8000/api/users/', self.user_register, format='json')
         self.assertEqual(response.status_code, 201)
     
     def test_user_login(self):
         """user login"""
-        response = self.api_client.post('http://localhost:8000/api/jwt/create/', self.user_login, format='json')
+        response = self.api_client.post('http://server:8000/api/jwt/create/', self.user_login, format='json')
         self.assertEqual(response.status_code, 401)
         error = json.loads(response.content)['detail']
         self.assertEqual(error, 'No active account found with the given credentials')
         self.user.is_active = True
         self.user.save()
-        response = self.api_client.post('http://localhost:8000/api/jwt/create/', self.user_login, format='json')
+        response = self.api_client.post('http://server:8000/api/jwt/create/', self.user_login, format='json')
         self.assertEqual(response.status_code, 200)
         self.token = json.loads(response.content)['access']
 
     def test_current_user(self):
         """user me"""
         self.api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
-        response = self.api_client.get('http://localhost:8000/api/users/me/', format='json')
+        response = self.api_client.get('http://server:8000/api/users/me/', format='json')
         self.assertEqual(response.status_code, 200)
