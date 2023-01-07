bc6b632d0be0821f814cc52bc1742066b08a5755
youssefMes <youssef-mestiri@outlook.fr>
2023-01-07 17:23:05 +0100

    add tests tag

--- server/scraperApp/tests.py
+++ server/scraperApp/tests.py
@@ -1,12 +1,13 @@
 import email
 from urllib import response
-from django.test import TestCase
+from django.test import TestCase, tag
 from .models import Bookmark, Course, Information, Portal, User
 from rest_framework.test import APIClient
 import json
 
-
+@tag('api')
 class CourseTestCase(TestCase):
+    
     @classmethod
     def setUpTestData(cls):
         portal_1 = Portal.objects.create(name='Test Portal 1' , link='https://test_1.com')
@@ -29,31 +30,30 @@ class CourseTestCase(TestCase):
         response = self.client.get('http://server:8000/api/courses/', {'search': 'Management'})
         for result in response.data['results']:
             self.assertIn('Management', result['name'])
-    
+
     def test_course_results_filter(self):
         """fitler fonctionalities"""
-        response = self.client.get('http://server:8000/api/courses/', {'city': 'CITY_1'})
+        response = self.client.get('http://server:8000/api/courses/', {'cities': ['CITY_1']})
         self.assertEqual(len(response.data['results']), 1)
         for result in response.data['results']:
             self.assertIn('CITY_1', result['information']['city'])    
 
-        response = self.client.get('http://server:8000/api/courses/', {'city': 'CITY_1', 'portal': 'Test Portal 1'})
+        response = self.client.get('http://server:8000/api/courses/', {'cities': 'CITY_1', 'portals': 'Test Portal 1'})
         self.assertEqual(len(response.data['results']), 1)
         self.assertIn('CITY_1', response.data['results'][0]['information']['city'])
         self.assertEqual('Test Portal 1', response.data['results'][0]['portal']['name'])
 
-        response = self.client.get('http://server:8000/api/courses/', {'city': 'CITY_1', 'portal': 'Test Portal 2'})
+        response = self.client.get('http://server:8000/api/courses/', {'cities': 'CITY_1', 'portals': 'Test Portal 2'})
         self.assertEqual(len(response.data['results']), 0)
-        
+
     def test_course_pagination(self):
         """ Pagination """
         response = self.client.get('http://server:8000/api/courses/', {'limit': 2})
         self.assertIn('page=2', response.data['next'])
         self.assertEqual(3, response.data['count'])
 
-
+@tag('api')    
 class BookmarkTestCase(TestCase):
-    
     @classmethod
     def setUpTestData(cls):
         cls.api_client = APIClient()
@@ -79,7 +79,6 @@ class BookmarkTestCase(TestCase):
         response = self.api_client.post('http://server:8000/api/bookmarks/', {'user': self.user.id, 'course': self.course_3.id}, format='json')
         self.assertEqual(response.status_code, 201)
 
-
     def test_bookmarks_listing(self):
         """retrieve user bookmarks"""
         self.api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
@@ -89,9 +88,8 @@ class BookmarkTestCase(TestCase):
         self.assertIn('page=1', response.data['previous'])
 
 
-
+@tag('api')       
 class UserTestCase(TestCase):
-    
     @classmethod
     def setUpTestData(cls):
         cls.api_client = APIClient()
@@ -117,7 +115,7 @@ class UserTestCase(TestCase):
         user.save()
         response = cls.api_client.post('http://server:8000/api/jwt/create/', cls.current_user , format='json')
         cls.token = response.data['access']
-                
+
     def test_user_registration(self):
         """user register"""
         response = self.api_client.post('http://server:8000/api/users/', self.user_register, format='json')
