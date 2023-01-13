dc51e275093f3a068c772e5aa3ba58d973e1fb1c
youssefMes <youssef-mestiri@outlook.fr>
2023-01-13 16:25:24 +0100

    download delay, nginx default conf

--- server/scraper/settings.py
+++ server/scraper/settings.py
@@ -68,6 +68,7 @@ ITEM_PIPELINES = {
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
+DOWNLOAD_DELAY=1
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
